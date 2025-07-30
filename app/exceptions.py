8a89888e6330a9d119522d3d7944f0052862475d
hunnidRose <hunnidrose@gmail.com>
2025-07-30 23:36:02 +0400

    Added base exception class

---
+++ app/exceptions.py
@@ -0,0 +1,7 @@
+from typing import Optional
+
+
+class ApplicationError(BaseException):
+    def __init__(self, message: str, extra_info: Optional[object] = None):
+        super().__init__(message)
+        self.extra_info = extra_info
