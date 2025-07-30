e32a9c854fc590ae83d93c4b4f07f7ab31cc660b
hunnidRose <hunnidrose@gmail.com>
2025-07-31 01:56:12 +0400

    Added exceptions for processing added words

--- app/exceptions.py
+++ app/exceptions.py
@@ -5,3 +5,12 @@ class ApplicationError(BaseException):
     def __init__(self, message: str, extra_info: Optional[object] = None):
         super().__init__(message)
         self.extra_info = extra_info
+
+
+class NoDataAvailableError(ApplicationError): ...
+
+
+class TooMuchDataError(ApplicationError): ...
+
+
+class WordLengthError(ApplicationError): ...
